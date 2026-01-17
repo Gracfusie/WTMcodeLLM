63f588860f1c4e12609f2febd03fd9eae5a9d957
Bowei Zhang <hbzbw001@yeah.net>
2026-01-17 20:28:57 +0800

    修复 env.sh vllm_use_modelscope 没有生效的 bug

--- env.sh
+++ env.sh
@@ -4,10 +4,10 @@ source .venv/bin/activate
 # 确保权重和缓存都落在大盘
 export HF_HOME=/root/autodl-tmp/hf
 export HUGGINGFACE_HUB_CACHE=/root/autodl-tmp/hf
-export VLLM_USE_MODELSCOPE=1
+export VLLM_USE_MODELSCOPE=true
 export MODELSCOPE_CACHE=/root/autodl-tmp/modelscope_cache
 export VLLM_WORKDIR=/root/autodl-tmp/vllm_workdir   # 可选，编译/缓存目录
-export HF_HUB_DISABLE_XET=1  # 临时解决 bug
+export HF_HUB_DISABLE_XET=true  # 临时解决 bug
 export TIKTOKEN_ENCODINGS_BASE=/root/autodl-tmp/tiktoken_rs_cache
 export PYTHONPATH=$(pwd):${PYTHONPATH}
 
@@ -24,4 +24,4 @@ export WATERMARK_SECRET_KEY="42"
 
 # Proxy 模型的 Window Size. -1 表示不使用 Window
 # 启动的话可能会让水印更 robust, i.e. 不容易靠截断/移动代码片段攻击
-export WATERMARK_PROXY_WINDOW_SIZE="-1"
+export WATERMARK_PROXY_WINDOW_SIZE="256"
